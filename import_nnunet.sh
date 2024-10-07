e9040de068db6a54619186329d2a19c067fa9d60
bsin0008 <89754299+bsin0008@users.noreply.github.com>
2024-10-07 17:22:32 +1100

    Create import_nnunet.sh
    This will set up the nnUNet with PINGU weights on your machine.
    
    Prerequisites:
    1) set up PINGU_env conda environment
    2) download PINGU weights: 10.26180/27176523

---
+++ import_nnunet.sh
@@ -0,0 +1,32 @@
+# Perivascular space Identification Nnunet for Generalaisable Usage (PINGU)
+# Usage Instructions: Noot noot noot noot noot, Noot noot noot noot noot, Noot noot noot noot noot noot noot noot noot noot noot noot noot noot, Noot noot noot noot. NOOT NOOT!
+#======================================
+
+
+
+conda activate PINGU_env
+
+
+PINGU_version='PINGU-All'
+
+if [ $PINGU_version == 'PINGU-All' ]; then
+	dataset=All
+elif [ $PINGU_version == 'PINGU-AF' ]; then
+	dataset=AFonly
+elif [ $PINGU_version == 'PINGU-ADNI' ]; then 
+	dataset=ADNIonly
+fi
+echo dataset: $dataset
+#====================================== End Options
+
+weightsdir=/home/bsin0008/Documents/Work/Projects/PINGU/PINGU-All
+
+
+# point to nnUNet weights
+export nnUNet_raw_data_base=$weightsdir/nnUNet_raw_data_base
+export nnUNet_preprocessed=$weightsdir/nnUNet_preprocessed
+export RESULTS_FOLDER=$weightsdir/nnUNet_trained_models
+
+# Lets go fishing
+nnUNet_install_pretrained_model_from_zip PINGU-All_v0.zip
+
